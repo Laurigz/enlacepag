cbc32593c34b166272050446c5eac4558b2841ac
Laurigz <laure@user.com>
2026-03-28 19:53:37 -0300

    fix(build): configure multiple entry points for all html pages

--- vite.config.ts
+++ vite.config.ts
@@ -12,4 +12,19 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./"),
     },
   },
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+        blog: path.resolve(__dirname, 'blog.html'),
+        contacto: path.resolve(__dirname, 'contacto.html'),
+        nosotros: path.resolve(__dirname, 'nosotros.html'),
+        portfolio: path.resolve(__dirname, 'portfolio.html'),
+        servicioBranding: path.resolve(__dirname, 'servicio-branding.html'),
+        servicioIa: path.resolve(__dirname, 'servicio-ia.html'),
+        servicioPosicionamiento: path.resolve(__dirname, 'servicio-posicionamiento.html'),
+        servicioWeb: path.resolve(__dirname, 'servicio-web.html'),
+      }
+    }
+  }
 })
